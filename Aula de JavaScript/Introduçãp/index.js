1befdd7edafd2a1d8c5d0df76b7f0b6861522a93
Ricardo Conesa <ricardo.conesa@gmail.com>
2025-08-19 11:52:33 -0300

    aula java

---
+++ Aula de JavaScript/Introduçãp/index.js
@@ -0,0 +1,15 @@
+//mensagem exibida no consele
+console.log("Digite o Nome do personagem")
+
+//variavel
+let nome = "Ricardo"
+
+//mensagem fixa mais concatenar com nome
+console.log("bem vindo " + nome)
+console.log(nome + " entrou no servidor.... ")
+
+//contante valor que nao é auteravel
+
+const notificacao = "Notificação para o Mestre: "
+
+console.log(notificacao + "Voce esta em perigo ")
