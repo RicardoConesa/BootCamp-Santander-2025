78370544c9651d6784766f39d10e7b3964ed24bc
Ricardo Conesa <ricardo.conesa@gmail.com>
2025-08-19 12:35:34 -0300

    aula java

--- Aula de JavaScript/Introduçãp/index.js
+++ Aula de JavaScript/Introduçãp/index.js
@@ -13,3 +13,21 @@ console.log(nome + " entrou no servidor.... ")
 const notificacao = "Notificação para o Mestre: "
 
 console.log(notificacao + "Voce esta em perigo ")
+
+// proximo exercicio
+
+const hojetem = "Na cozinha da vovo Hoje tem"
+let poteCafe = "Pote de Café"
+let poteAcucar = "Pode de Açucar"
+let biscoiutoMaizena = "Pote de Biscoito de Maizena"
+console.log(hojetem)
+console.log( poteCafe +"")
+console.log( poteAcucar +"")
+console.log( biscoiutoMaizena +"")
+
+// Fixação de variaveis 
+
+let idade = 18
+let nomea = "Ricardo Conesa"
+let telefone = "14 98888-0000"
+let email = "email@provedor.com.br"
